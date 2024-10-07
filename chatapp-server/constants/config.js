63f8f92010fcaac314ed99c548db178b87dcc710
Sanskar Gupta <sanskargupta9415@gmail.com>
2024-10-08 00:33:41 +0530

    update/cors

--- chatapp-server/constants/config.js
+++ chatapp-server/constants/config.js
@@ -3,6 +3,8 @@ const corsOptions = {
     "http://localhost:5173",
     "http://localhost:4173",
     process.env.CLIENT_URL,
+    "https://echoverse-chatapp-frontend.onrender.com",
+    "https://echoverse-chatapp-backend.onrender.com",
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
